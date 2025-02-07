3875704ab587c7ad82bef8cc977f0ad89f6aa657
Lishan Weerasuriya <lishanweerasuriya@gmail.com>
2025-02-07 16:36:11 +0530

    Create Feature Component

---
+++ src/FeatureCard.jsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/Card.jsx";
+
+function FeatureCard({ title, description, Icon }) {
+  return (
+    <Card className="border-none shadow-md hover:shadow-lg transition-shadow">
+      <CardHeader>
+        <Icon className="w-10 h-10 text-green-600 mb-2" />
+        <CardTitle className="text-xl p-2">{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="p-2 text-muted-foreground">{description}</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default FeatureCard;
